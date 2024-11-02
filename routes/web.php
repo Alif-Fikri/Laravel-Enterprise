3aaf591f05912eb3a4aaa0e7cebb7f3138e4b861
Alif-Fikri <aliefmaulanafikri@gmail.com>
2024-11-02 17:47:43 +0700

    Update CRUD

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\contohcontroller;
 use App\Http\Controllers\ProductController;
+use App\Models\Product;
 
 Route::get('/', function () {
     return view('welcome');
@@ -25,3 +26,8 @@ Route::get('/contoh',[contohcontroller::class,'tampilcontoh']);
 //     return view('product');
 // });
 Route::get('/product',[ProductController::class,'ViewProduct']);
+Route::get('/product/add',[ProductController::class,'ViewAddProduct']);
+Route::post('/product/add',[ProductController::class,'CreateProduct']);
+Route::delete('product/delete/{kode_produk}', [ProductController::class, 'DeleteProduct']);
+Route::get('/product/edit/{kode_produk}',[ProductController::class, 'ViewEditProduct']);
+Route::put('/product/edit/{kode_produk}',[ProductController::class, 'UpdateProduct']);
